9e4cd2f024dda834511856291321c4c5ed8dab40
Stuyk <stuykgaming@gmail.com>
2018-09-16 14:44:07 -0700

    Bank Changes

--- bridge/resources/WiredPlayers/bank/Bank.cs
+++ bridge/resources/WiredPlayers/bank/Bank.cs
@@ -13,113 +13,182 @@ namespace WiredPlayers.bank
         [RemoteEvent("executeBankOperation")]
         public void ExecuteBankOperationEvent(Client player, int operation, int amount, String targetName)
         {
-            String response = String.Empty;
+            // Throw an error if the amount is less than zero.
+            if (amount <= 0)
+            {
+                TriggerLocalResponse(player, Messages.ERR_BANK_GENERAL_ERROR);
+                return;
+            }
+
+            // Figure out the action of the bank operation.
+            switch (operation)
+            {
+                case Constants.OPERATION_WITHDRAW:
+                    WithdrawFromBank(player, amount);
+                    return;
+                case Constants.OPERATION_DEPOSIT:
+                    DepositToBank(player, amount);
+                    return;
+                case Constants.OPERATION_TRANSFER:
+                    TransferFromBank(player, amount, targetName);
+                    return;
+                case Constants.OPERATION_BALANCE:
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// Update the player's shared data for money, bank, etc. Setting money to -99 will not update the money for the target.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="bank"></param>
+        /// <param name="money"></param>
+        private void UpdatePlayerMoney(Client player, int bank, int money = -99)
+        {
+            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_BANK, bank);
+
+            if (money == -99)
+                return;
+
+            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_MONEY, money);
+        }
+
+        /// <summary>
+        /// Trigger a client event that tells the player what happened.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="response"></param>
+        private void TriggerLocalResponse(Client player, string response)
+        {
+            NAPI.ClientEvent.TriggerClientEvent(player, "bankOperationResponse", response);
+        }
+
+        /// <summary>
+        /// Withdraws from the bank based on the player's input.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="bank"></param>
+        /// <param name="amount"></param>
+        private void WithdrawFromBank(Client player, int amount)
+        {
+            // Get Player Money, Bank Info, Name, etc.
             int bank = NAPI.Data.GetEntitySharedData(player, EntityData.PLAYER_BANK);
             int money = NAPI.Data.GetEntitySharedData(player, EntityData.PLAYER_MONEY);
             String name = NAPI.Data.GetEntityData(player, EntityData.PLAYER_NAME);
 
-            // Checking that amount is valid
-            if (amount > 0)
+            // If the bank has less than the amount requested. Throw an error.
+            if (bank < amount)
             {
-                switch (operation)
+                TriggerLocalResponse(player, Messages.ERR_BANK_NOT_ENOUGH_MONEY);
+                return;
+            }
+
+            // Update data and then push it to the player's shared data.
+            bank -= amount;
+            money += amount;
+            UpdatePlayerMoney(player, bank, money);
+
+            // Log the transaction to the database.
+            Task.Factory.StartNew(() =>
+            {
+                Database.LogPayment("ATM", name, Messages.GEN_BANK_OP_WITHDRAW, amount);
+            });
+        }
+
+        /// <summary>
+        /// Deposit money into the bank based on exceeding amount or whatever amount the player provides.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="amount"></param>
+        private void DepositToBank(Client player, int amount)
+        {
+            // Get Player Money, Bank Info, Name, etc.
+            int bank = NAPI.Data.GetEntitySharedData(player, EntityData.PLAYER_BANK);
+            int money = NAPI.Data.GetEntitySharedData(player, EntityData.PLAYER_MONEY);
+            String name = NAPI.Data.GetEntityData(player, EntityData.PLAYER_NAME);
+
+            // If the player's money is less than the amount he wants to deposit. Just move all of the player's money into the bank.
+            if (money < amount)
+            {
+                bank += money;
+                UpdatePlayerMoney(player, bank, money);
+
+                // We log the transaction into the database
+                Task.Factory.StartNew(() =>
                 {
-                    case Constants.OPERATION_WITHDRAW:
-                        if (bank >= amount)
-                        {
-                            bank -= amount;
-                            money += amount;
-                            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_BANK, bank);
-                            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_MONEY, money);
-
-                            Task.Factory.StartNew(() => {
-                                // We log the transaction into the database
-                                Database.LogPayment("ATM", name, Messages.GEN_BANK_OP_WITHDRAW, amount);
-                            });
-                        }
-                        else
-                        {
-                            response = Messages.ERR_BANK_NOT_ENOUGH_MONEY;
-                        }
-                        break;
-                    case Constants.OPERATION_DEPOSIT:
-                        if (money >= amount)
-                        {
-                            bank += amount;
-                            money -= amount;
-                            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_BANK, bank);
-                            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_MONEY, money);
-
-                            Task.Factory.StartNew(() => {
-                                // We log the transaction into the database
-                                Database.LogPayment("ATM", name, Messages.GEN_BANK_OP_DEPOSIT, amount);
-                            });
-                        }
-                        else
-                        {
-                            bank += money;
-                            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_BANK, bank);
-                            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_MONEY, 0);
-
-                            Task.Factory.StartNew(() => {
-                                // We log the transaction into the database
-                                Database.LogPayment(name, "ATM", Messages.GEN_BANK_OP_DEPOSIT, money);
-                            });
-                        }
-                        break;
-                    case Constants.OPERATION_TRANSFER:
-                        if (bank < amount)
-                        {
-                            response = Messages.ERR_BANK_NOT_ENOUGH_MONEY;
-                        }
-                        else
-                        {
-                            Task.Factory.StartNew(() => {
-                                if (Database.FindCharacter(targetName) == true)
-                                {
-                                    Client target = NAPI.Pools.GetAllPlayers().Find(x => x.Name == targetName);
-                                    if (target == player)
-                                    {
-                                        response = Messages.ERR_TRANSFER_MONEY_OWN;
-                                    }
-                                    else
-                                    {
-                                        if (target != null && NAPI.Data.HasEntityData(target, EntityData.PLAYER_PLAYING) == true)
-                                        {
-                                            int targetBank = NAPI.Data.GetEntitySharedData(target, EntityData.PLAYER_BANK);
-                                            targetBank += amount;
-                                            bank -= amount;
-                                            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_BANK, bank);
-                                            NAPI.Data.SetEntitySharedData(target, EntityData.PLAYER_BANK, targetBank);
-                                        }
-                                        else
-                                        {
-                                            bank -= amount;
-                                            NAPI.Data.SetEntitySharedData(player, EntityData.PLAYER_BANK, bank);
-                                            Database.TransferMoneyToPlayer(targetName, amount);
-                                        }
-
-                                        // We log the transaction into the database
-                                        Database.LogPayment(name, targetName, Messages.GEN_BANK_OP_TRANSFER, amount);
-                                    }
-                                }
-                                else
-                                {
-                                    response = Messages.ERR_BANK_ACCOUNT_NOT_FOUND;
-                                }
-                            });
-                            
-                        }
-                        break;
-                    case Constants.OPERATION_BALANCE:
-                        break;
-                }
+                    Database.LogPayment(name, "ATM", Messages.GEN_BANK_OP_DEPOSIT, money);
+                });
+                return;
             }
-            else
+
+            // If the amount is less than the player's money deposit the amount instead.
+            bank += amount;
+            money -= amount;
+            UpdatePlayerMoney(player, bank, money);
+
+            // We log the transaction into the database
+            Task.Factory.StartNew(() =>
             {
-                response = Messages.ERR_BANK_GENERAL_ERROR;
+                Database.LogPayment("ATM", name, Messages.GEN_BANK_OP_DEPOSIT, amount);
+            });
+        }
+
+        private void TransferFromBank(Client player, int amount, string targetPlayer)
+        {
+            // Get Player Money, Bank Info, Name, etc.
+            int bank = NAPI.Data.GetEntitySharedData(player, EntityData.PLAYER_BANK);
+            int money = NAPI.Data.GetEntitySharedData(player, EntityData.PLAYER_MONEY);
+            String name = NAPI.Data.GetEntityData(player, EntityData.PLAYER_NAME);
+
+            // If the bank has less than the amount requested. End it here.
+            if (bank < amount)
+            {
+                TriggerLocalResponse(player, Messages.ERR_BANK_NOT_ENOUGH_MONEY);
+                return;
+            }
+            
+            // Check if the account exists before we begin transferring.
+            if (Database.FindCharacter(targetPlayer) != true)
+            {
+                TriggerLocalResponse(player, Messages.ERR_BANK_ACCOUNT_NOT_FOUND);
+                return;
             }
 
-            NAPI.ClientEvent.TriggerClientEvent(player, "bankOperationResponse", response);
+            Client target = NAPI.Pools.GetAllPlayers().Find(x => x.Name == targetPlayer);
+
+            // If the target player is the client transferring stop it.
+            if (target == player)
+            {
+                TriggerLocalResponse(player, Messages.ERR_TRANSFER_MONEY_OWN);
+                return;
+            }
+
+            // If the target is null they're probably not on the server currently.
+            if (target == null)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    bank -= amount;
+                    UpdatePlayerMoney(player, bank, money);
+                    Database.TransferMoneyToPlayer(targetPlayer, amount);
+                    Database.LogPayment(name, targetPlayer, Messages.GEN_BANK_OP_TRANSFER, amount);
+                });
+                return;
+            }
+
+            if (NAPI.Data.HasEntityData(target, EntityData.PLAYER_PLAYING) == true)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    int targetBank = NAPI.Data.GetEntitySharedData(target, EntityData.PLAYER_BANK);
+                    targetBank += amount;
+                    bank -= amount;
+                    UpdatePlayerMoney(player, bank, money);
+                    UpdatePlayerMoney(target, targetBank);
+                });
+            }
         }
 
         [RemoteEvent("loadPlayerBankBalance")]
